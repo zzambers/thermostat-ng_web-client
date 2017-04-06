65055c5e35e6a286a796f26d5e64fef65b0b459a
Andrew Azores <aazores@redhat.com>
2017-04-06 11:11:28 -0400

    Add junit-reporter config for CI test compat
    Reviewed-by: jkang
    Review-thread: http://icedtea.classpath.org/pipermail/thermostat/2017-April/022636.html

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,11 @@ module.exports = function (config) {
       'test/**.test.js': ['babel']
     },
 
-    reporters: ['mocha', 'beep'],
+    reporters: ['mocha', 'beep', 'junit'],
+
+    junitReporter: {
+      outputDir: 'test-reports'
+    },
 
     exclude: [],
 
