96bb4d2390606f2e51e7491355e5161b236d84f2
Andrew Azores <aazores@redhat.com>
2017-06-20 11:24:50 -0400

    Integration test login helper supports both StubAuth and Keycloak
    Reviewed-by: jkang
    Review-thread: http://icedtea.classpath.org/pipermail/thermostat/2017-June/023572.html

--- integration-test/login-helper.js
+++ integration-test/login-helper.js
@@ -1,14 +1,31 @@
-function login () {
+module.exports = function () {
   var EC = protractor.ExpectedConditions;
 
+  browser.ignoreSynchronization = true;
   browser.driver.get(browser.params.webClientUrl);
 
+  var logout = element(by.id('logoutButton'));
+  browser.isElementPresent(logout).then(function (present) {
+    if (present) {
+      logout.isDisplayed().then(function (displayed) {
+        if (displayed) {
+          logout.click();
+        }
+      });
+    }
+  });
+
   var username = element(by.id('username'));
-  browser.wait(EC.visibilityOf(username), 5000);
+  browser.wait(EC.visibilityOf(username), 10000);
 
   username.sendKeys('test-user');
   element(by.id('password')).sendKeys('test-pass');
   element(by.id('kc-login')).click();
-}
 
-module.exports = login;
+  browser.wait(EC.visibilityOf(element(by.id('brandLogoImg')), 10000));
+
+  browser.call(function () {
+    browser.ignoreSynchronization = false;
+  });
+  browser.waitForAngular();
+}
